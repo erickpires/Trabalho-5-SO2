3a1fec469e059526fc6ab3f94270067a74af410a
Erick Pires <pires@erickpires.com>
2016-01-12 14:24:44 -0200

    Exercicio 3

--- exercicio_3.sh
+++ exercicio_3.sh
@@ -2,6 +2,11 @@
 
 directory="/tmp"
 
+if ! ls ${directory}/*.mpeg3 2> /dev/null > /dev/null; then
+	echo "Nenhum arquivo para alterar"
+	exit 0
+fi
+
 for file in ${directory}/*.mpeg3; do
 	new_name=$(echo $file | sed 's/mpeg3/mp3/')
 	mv "$file" "$new_name"
