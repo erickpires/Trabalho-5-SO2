abdbb8a2a9c4896ee61d3e6fece408c7844b7740
Erick Pires <pires@erickpires.com>
2016-01-12 14:23:52 -0200

    Exercicio 4

--- exercicio_4.sh
+++ exercicio_4.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+echo "Entre o diretório para alterar."
+read directory
+
+if [ ! -d $directory ]; then
+	echo "Entre com um diretório válido"
+	exit 1
+fi
+
+if ! ls ${directory}/*.mpeg3 2> /dev/null > /dev/null; then
+	echo "Nenhum arquivo para alterar"
+	exit 0
+fi
+
+for file in ${directory}/*.mpeg3; do
+	new_name=$(echo $file | sed 's/mpeg3/mp3/')
+	mv "$file" "$new_name"
+done
+
