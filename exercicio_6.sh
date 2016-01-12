c8ce474748a88fc3d237faed0e4fa57dc41ea1bf
Erick Pires <pires@erickpires.com>
2016-01-12 15:01:15 -0200

    Exercicio 6

--- exercicio_6.sh
+++ exercicio_6.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+directory=$1
+
+if [ ! -d "$directory" ]; then
+	echo -e "Usage: $0 \033[1;32mdirectory\033[0m"
+	exit 1
+fi
+
+counter=1
+for file in $directory/*; do
+	echo "$counter: $(basename "$file")"
+	counter=$(($counter+1))
+done
