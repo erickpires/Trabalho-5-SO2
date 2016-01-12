337cb06aec9d92d156134377009f05bf321eaeb1
Erick Pires <pires@erickpires.com>
2016-01-12 15:09:04 -0200

    Exercicio 7

--- exercicio_7.sh
+++ exercicio_7.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+file=$1
+
+if [ ! -f "$file" ]; then
+	echo "You must specify a valid file"
+	exit 1
+fi
+
+words=$(wc -w "$file" | cut -d " " -f 1)
+lines=$(wc -l "$file" | cut -d " " -f 1)
+
+echo -e "This file has \033[1;34m$words\033[0m words and \033[1;34m$lines\033[0m lines"
