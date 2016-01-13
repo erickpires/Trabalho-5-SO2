fb003aa8a4dab7975f6f489b7f0bfe33ae26279f
Matheus C. Ambrozio <m.c.ambrozio@gmail.com>
2016-01-13 08:32:38 -0200

    exercicio 1

--- exercicio_1.sh
+++ exercicio_1.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+filename=$1
+
+stat --format=%s $filename > /dev/null 2>&1
+return=$?
+
+if [ $return -eq 1 ]; then
+    echo "File \"$filename\" does not exist."
+    exit
+fi
+
+size=$(stat --format=%s $filename)
+
+if [ $size -gt 100 ]; then
+    echo "File \"$filename\" has more than 100 bytes ($size bytes)"
+else
+    echo "File \"$filename\" has less than 100 bytes ($size bytes)"
+fi
