a3c1acc9950aeabbfe23fe94b4cf7d23f833af08
Matheus C. Ambrozio <m.c.ambrozio@gmail.com>
2016-01-13 08:32:52 -0200

    exercicio 10

--- exercicio_10.sh
+++ exercicio_10.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+find / -type f -user $USERNAME > arq.txt 2> /dev/null
+
+find / -type d -user $USERNAME > dir.txt 2> /dev/null
+
+cat arq.txt > lista.txt
+cat dir.txt >> lista.txt
+
+rm arq.txt dir.txt
