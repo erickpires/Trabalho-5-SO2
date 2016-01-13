122be54ea08541c7f00afb49715b6298b1a86704
Matheus C. Ambrozio <m.c.ambrozio@gmail.com>
2016-01-13 08:32:57 -0200

    exercicio 11

--- exercicio_11.sh
+++ exercicio_11.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+users=$(cut -d: -f1,3 /etc/passwd)
+
+for user in $users; do
+    uid=$(echo $user | cut -d: -f2)
+    if [ $uid -gt 100 ]; then
+        username=$(echo $user | cut -d: -f1)
+        echo "$username ($uid)"
+    fi
+done
